2a8455e717834cd1d1319516054ffaa501a3b2dd
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-10-28 17:53:54 +0000

    atualizando

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,13 +6,13 @@ async function vizualizarInformacoesGlobais() {
     //console.log(dados)
 
     const pessoasNoMundo = (dados.total_pessoas_mundo/ 1e9)
-    const pessoasConectadas = (dados.total_pessoas_empregadas/1e9) 
+    const pessoasEmpregadas = (dados.total_pessoas_empregadas/1e9) 
     const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
     const minutos = Math.round ((dados.tempo_medio_trabalho_por_semana - horas)* 60)
     
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} </span> de pessoas e que aproximadamente <span> ${pessoasEmpregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${horas} </span> horas e <span> ${minutos} minutos </span> por semana.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
