00c404261ec3898224fdcfe6b029683ab57f1d65
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-10-28 17:47:05 +0000

    numeros

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,6 +4,11 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     //console.log(dados)
+
+    const pessoasNoMundo = (dados.total_pessoas_mundo/ 1e9)
+    const pessoasConectadas = (dados.total_pessoas_empregadas/1e9) 
+    const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
+    const minutos = Math.round ((dados.tempo_medio_trabalho_por_semana - horas)* 60)
     
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
