18310047a0c376c09955bcadf8a3e0123f87c921
Louisebgoes <115102707+Louisebgoes@users.noreply.github.com>
2024-11-11 15:28:45 -0300

    atualizando

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-tipos-de-ocupacao.json'
     const res = await fetch(url)
@@ -9,14 +11,48 @@ async function quantidadeUsuarios() {
     {
       x: nomeDasRedes,
       y: quantidadeUsuarios,
-      type: 'bar'
+      type: 'bar',
+      marker: {
+        color: getCSS('--primary-color'),
+        }
     }
   ]
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: "Tipos de trabalhos realizados",
+      font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30,
+      }
+   },
+   xaxis: {
+    tickfont: tickConfig,
+    title: {
+       text: "Tipos de trabalhos",
+       font: {
+          color: getCSS('--secundary-color')
+      }
+   }
+ },
+ yaxis: {
+  tickfont: tickConfig,
+  title: {
+     text: "Números de pessoas",
+     font: {
+        color: getCSS('--secundary-color')
+    }
+ }
+}
+}
   
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+  Plotly.newPlot(grafico, data, layout)
   
   }
   
